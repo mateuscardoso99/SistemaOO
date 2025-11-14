92ff9c05953a98e8b2f986ae3f429ca30818ce09
Jonas <jonassilvadelara@gmail.com>
2025-11-14 12:23:32 -0300

    chore: configurar sonar cloud

--- filmatoca/src/App.java
+++ filmatoca/src/App.java
@@ -1,5 +1,11 @@
+import java.time.LocalDateTime;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    public static void badMethod(){
+        LocalDateTime now;
     }
 }
