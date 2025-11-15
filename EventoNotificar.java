5f4fbcb0dd6c9c82bf66d67731c7e5c801bcba72
mat475000 <mat358591@gmail.com>
2025-11-15 17:39:31 -0300

    padrão observer

---
+++ EventoNotificar.java
@@ -0,0 +1,14 @@
+public enum EventoNotificar {
+    NOVO_FILME("Novo filme"),
+    FILME_DEVOLVIDO("Filme devolvido");
+
+    private String value;
+
+    private EventoNotificar(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
---
+++ Ouvinte.java
@@ -0,0 +1,3 @@
+public interface Ouvinte {
+    void update(String msg);
+}
